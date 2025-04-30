79f89170f58412a1d80bac8c612ba35c37b7a4c8
97mams <97mams@gmail.com>
2025-04-30 10:45:12 -0700

    add diasyUi

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+   import "../style.css";
+</script>
+
+<div>
+  <button class="btn btn-success">hellow</button>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus eos explicabo esse id suscipit inventore. Aliquid, distinctio quam assumenda cum ab sunt, asperiores voluptatum, quas corrupti repellendus similique odit ipsam?</p>
+</div>
+
