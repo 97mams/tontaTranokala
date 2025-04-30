983cf8e0849baa29223f419fea155324510302ec
97mams <97mams@gmail.com>
2025-04-30 12:14:38 -0700

    changed title

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,8 +1,13 @@
 <div class="navbar bg-base-100 shadow-sm">
   <div class="flex-1">
-    <a href="/" class="btn btn-ghost text-xl">daisyUI</a>
+    <a href="/" class="btn btn-ghost text-xl">tanokalaKo</a>
   </div>
   <div class="flex gap-2">
     <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
   </div>
+  <ul>
+    <li>
+      <a href="/create">Ajouter</a>
+    </li>
+  </ul>
 </div>
