3bc58499edacb93af152ac360ac4ed913d55d50f
97mams <97mams@gmail.com>
2025-05-05 10:04:18 -0700

    fix types

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,13 +1,16 @@
 import type { Actions } from "./$types.js";
+import { createSites, type dataType } from "./runes/site.svelte.js";
 
 export const actions = {
   default: async ({request}) => {
     const formData = await request.formData()
-    const data = {
-      siteName: formData.get('name'),
-      url: formData.get('url'),
-      description: formData.get('description'),
+    const data:dataType = {
+      name: String(formData.get('name')),
+      url: String(formData.get('url')),
+      description:String(formData.get('description')),
     }
+    const sites = createSites()
+    sites.addSite(data)
     return {
       success: true
     }
