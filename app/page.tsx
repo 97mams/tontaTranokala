d25c718fe94c015605ee62bc7074ac01fb1c8b76
97mams <97mams@gmail.com>
2025-07-24 16:53:13 +0300

    swich project to nextjs 😁

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Register for link web page</h1>
+      <p>This is the main content of the page.</p>
+    </div>
+  );
+}
--- src/components/Footer.svelte
+++
@@ -1,5 +0,0 @@
-<footer class="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
-  <aside>
-    <p>Copyright © {new Date().getFullYear()} </p>
-  </aside>
-</footer>
