73faf122fa0650203b9a91decbd9d6a6f6e171d8
97mams <97mams@gmail.com>
2025-07-25 06:10:56 +0300

    config prima and zod

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,24 @@
+import { ThemeProvider } from "@/components/ui/theme-provider"
+import { ModeToggle } from "@/components/ui/theme-toggle"
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider 
+          attribute="class" 
+          defaultTheme="system" 
+          enableSystem={true}
+          disableTransitionOnChange={true}
+        >
+          <ModeToggle />
+          {children}
+        </ThemeProvider>
+        </body>
     </html>
   )
 } 
