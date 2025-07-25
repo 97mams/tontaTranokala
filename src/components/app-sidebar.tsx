3ee4dc843a03ab4d935795a543dbace4ed329489
97mams <97mams@gmail.com>
2025-07-25 15:54:57 +0300

    add sidebar shadcnui

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,20 @@
+import { SidebarGroupLabel, SidebarGroupContent,SidebarGroup } from "./ui/sidebar"
+import { ChevronDown } from "lucide-react"
+
+export function AppSidebar() {
+  return (
+    <Collapsible defaultOpen className="group/collapsible">
+      <SidebarGroup>
+        <SidebarGroupLabel asChild>
+          <CollapsibleTrigger>
+            Help
+            <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+          </CollapsibleTrigger>
+        </SidebarGroupLabel>
+        <CollapsibleContent>
+          <SidebarGroupContent />
+        </CollapsibleContent>
+      </SidebarGroup>
+    </Collapsible>
+  )
+}
