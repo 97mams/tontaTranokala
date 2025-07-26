b6ae6f1b019b38a4520d7aa84b46e3c8024806ae
97mams <97mams@gmail.com>
2025-07-26 07:01:01 +0300

    fix: sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css"
 import { AppSidebar } from "@/components/app-sidebar"
-import { SidebarProvider } from "@/components/ui/sidebar"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { ThemeProvider } from "@/components/ui/theme-provider"
 import { ModeToggle } from "@/components/ui/theme-toggle"
 import { Button } from "@/components/ui/button"
@@ -15,17 +15,17 @@ export default function RootLayout({
       <body>
         <ThemeProvider 
           attribute="class" 
-          defaultTheme="system" 
+          defaultTheme="treu" 
           enableSystem={true}
-          disableTransitionOnChange={true}
         >
           <SidebarProvider>
             <AppSidebar />
-            <main>
-              <ModeToggle />
-              {children}
-            </main>
-            <Button>Aza eh</Button>
+              <SidebarTrigger />
+              <main>
+                <ModeToggle />
+                {children}
+              </main>
+              <Button>Aza eh</Button>
           </SidebarProvider>
         </ThemeProvider>
         </body>
--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -2,23 +2,30 @@
 
 import { Collapsible } from "@radix-ui/react-collapsible"
 import { CollapsibleContent, CollapsibleTrigger } from "./ui/collapsible"
-import { SidebarGroupLabel, SidebarGroupContent,SidebarGroup } from "./ui/sidebar"
+import { SidebarGroupLabel, SidebarGroupContent,SidebarGroup, Sidebar } from "./ui/sidebar"
 import { ChevronDown } from "lucide-react"
 
 export function AppSidebar() {
   return (
-    <Collapsible defaultOpen className="group/collapsible">
-      <SidebarGroup>
-        <SidebarGroupLabel asChild>
-          <CollapsibleTrigger>
-            Help
-            <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
-          </CollapsibleTrigger>
-        </SidebarGroupLabel>
-        <CollapsibleContent>
-          <SidebarGroupContent />
-        </CollapsibleContent>
-      </SidebarGroup>
-    </Collapsible>
+    <Sidebar>
+      <Collapsible defaultOpen className="group/collapsible">
+        <SidebarGroup>
+          <SidebarGroupLabel asChild>
+            <CollapsibleTrigger>
+              Help
+              <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+            </CollapsibleTrigger>
+          </SidebarGroupLabel>
+          <CollapsibleContent>
+            <ul>
+              <li>mety </li>
+              <li>le izy</li>
+              <li>fa</li>
+              <li>rasy be</li>
+            </ul>
+          </CollapsibleContent>
+        </SidebarGroup>
+      </Collapsible>
+    </Sidebar>
   )
 }
