3df6dd4648969b3e2d437285c04098d4e03f1ca8
97mams <97mams@gmail.com>
2025-07-28 15:08:29 +0300

    add form action

---
+++ server/form-action.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { toast } from "sonner";
+import { z } from "zod";
+
+const site = z.object({
+  title: z.string().min(1, "Title is required"),
+  description: z.string().optional(),
+  url: z.string().optional,
+  });
+
+const group = z.object({
+  title: z.string().min(1, "Title is required")
+});
+
+export async function formGroupAction(formData: FormData) {
+
+  const title = formData.get("title") as string;
+
+  const input = group.safeParse({ title });
+  if (!input.success) {
+    return {error: true, message: input.error}
+  }
+
+  // const addSiteGroup = await prisma.groupSite.create({
+  //   data: {
+  //     title: input.data.title,
+  //   },
+  // });
+
+  // if (!addSiteGroup) {
+  //   return {error: true, message: "data not found"}
+  // }
+
+  return {success: true, message: input.success}
+}
