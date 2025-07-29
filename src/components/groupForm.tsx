ebb23b16c0d271825216c16b32dd34f3a84a3ef0
97mams <97mams@gmail.com>
2025-07-29 07:34:55 +0300

    feta: add toaster sonner

--- src/components/groupForm.tsx
+++ src/components/groupForm.tsx
@@ -14,12 +14,20 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { formGroupAction } from "../../server/form-action"
+import { toast } from "sonner"
 
 export function GroupeSiteForm() {
 
    const handlerSubmit = (formatData: FormData) => {
       formGroupAction(formatData)
-      .then(r => console.log(r))
+      .then(r => {
+         if (r.error) {
+            toast.error("vérifier le champs ..")
+         }
+         if (r.success) {
+            toast.success("Ajout réussir..")
+         }
+      })
    }
 
    return(
@@ -37,18 +45,20 @@ export function GroupeSiteForm() {
                   </DialogDescription>
                </DialogHeader>
                <form action={handlerSubmit}>
-               <div className="grid gap-4 my-2">
-                  <div className="grid gap-3">
-                  <Label htmlFor="title">Titre</Label>
-                  <Input id="title" name="title" placeholder="ex: Nextjs" required/>
+                  <div className="grid gap-4 my-2">
+                     <div className="grid gap-3">
+                     <Label htmlFor="title">Titre</Label>
+                     <Input id="title" name="title" placeholder="ex: Nextjs" required/>
+                     </div>
                   </div>
-               </div>
-               <DialogFooter>
-                  <DialogClose asChild>
-                  <Button variant="outline">Annuler</Button>
-                  </DialogClose>
-                  <Button type="submit">Enregister</Button>
-               </DialogFooter>
+                  <DialogFooter>
+                     <DialogClose asChild>
+                        <Button variant="outline">Annuler</Button>
+                     </DialogClose>
+                     <DialogClose asChild>
+                        <Button type="submit">Enregister</Button>
+                     </DialogClose>
+                  </DialogFooter>
                </form>
             </DialogContent>
       </Dialog>
