421829b5f6f444a66adc15649d43794134109332
97mams <97mams@gmail.com>
2025-07-31 08:07:12 +0300

    fix: get params

--- app/site/[siteName]/page.tsx
+++ app/site/[siteName]/page.tsx
@@ -1,18 +1,18 @@
 import { SiteForm } from "@/components/siteForm"
-import { castToString } from "@/lib/urlHelper"
+import { castToString, stringToArray } from "@/lib/urlHelper"
 
 
 export default async function Page(props: {
   params:Promise<{siteName: string}>
 }) {
   const params = await props.params
-  const name = castToString(params.siteName)
+  const newParams = stringToArray(params.siteName)
   return(
   <div className="flex flex-col gap-2">
     <div className="flex gap-4">
-      <SiteForm name={name}/>
+      <SiteForm id={Number(newParams[0])}/>
     </div>
-    {name}
+    {castToString(newParams[1])}
     </div>
   )
 }
