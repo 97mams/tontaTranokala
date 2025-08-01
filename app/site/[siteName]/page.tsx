1c861ce204d3ac8811cdca1edd0e970a53ff572a
97mams <97mams@gmail.com>
2025-08-01 08:37:36 +0300

    feat: change component to component action

--- app/site/[siteName]/page.tsx
+++ app/site/[siteName]/page.tsx
@@ -1,4 +1,6 @@
+import { CardListSite } from "@/components/cardListSite"
 import { SiteForm } from "@/components/siteForm"
+import { prisma } from "@/lib/prisma"
 import { castToString, stringToArray } from "@/lib/urlHelper"
 
 
@@ -7,12 +9,33 @@ export default async function Page(props: {
 }) {
   const params = await props.params
   const newParams = stringToArray(params.siteName)
+
+  const sitesByGroupId = await prisma.site.findMany({
+    where: {GroupSiteId: Number(newParams[0])},
+    select: { id: true, name: true, description: true, url: true }
+  })
+
   return(
   <div className="flex flex-col gap-2">
-    <div className="flex gap-4">
+    <h1 className="scroll-m-20 uppercase text-4xl font-extrabold tracking-tight text-balance">  
+      {castToString(newParams[1])}
+    </h1>
+    <p className="leading-7 [&:not(:first-child)]:mt-6">
+      Ici, retrouvez tous les sites dédiés à {castToString(newParams[1])}.
+    </p>
+    <div>
+      { sitesByGroupId.map(site => (
+        <CardListSite 
+          key={site.id}
+          name={site.name}
+          desciption={site.description}
+          url={site.url}
+        />
+      )) }
+    </div>
+    <div className="flex gap-4 mt-8">
       <SiteForm id={Number(newParams[0])}/>
     </div>
-    {castToString(newParams[1])}
     </div>
   )
 }
