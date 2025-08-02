edebd06c8c554eac17cf1bf40d5c5716f98cae8b
97mams <97mams@gmail.com>
2025-08-02 17:31:47 +0300

    feat: liste for group more visited

---
+++ src/components/sideListProject.tsx
@@ -0,0 +1,26 @@
+import { prisma } from "@/lib/prisma";
+
+export async function TopProjects() {
+
+  const projects = await prisma.groupSite.findMany({
+    orderBy: {
+      visits: 'desc',
+
+
+
+  return (
+    <div className="w-md p-4">
+      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight mb-2">Top projets</h3>
+      <p className="text-muted-foreground text-sm">Retrouvez ici vos projets les plus utilisés.</p>
+      <ul className="mt-4 my-6 ml-6 list-disc [&>li]:mt-2">
+        {projects.map((project) => (
+          <li key={project.id}>
+            <a href={`/site/${project.id}-${project.title}`} className="hover:underline">
+              {project.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
