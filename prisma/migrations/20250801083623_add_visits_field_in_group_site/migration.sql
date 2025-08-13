38ac159937de6577d83e46f2e1d96e088f65b466
97mams <97mams@gmail.com>
2025-08-13 17:01:27 +0300

    feat: recreate migration files for GroupSite and site tables with updated schema

--- prisma/migrations/20250801083623_add_visits_field_in_group_site/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropIndex
-DROP INDEX `site_GroupSiteId_fkey` ON `site`;
-
--- AlterTable
-ALTER TABLE `groupsite` ADD COLUMN `visits` INTEGER NOT NULL DEFAULT 0;
-
--- AddForeignKey
-ALTER TABLE `site` ADD CONSTRAINT `site_GroupSiteId_fkey` FOREIGN KEY (`GroupSiteId`) REFERENCES `GroupSite`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
