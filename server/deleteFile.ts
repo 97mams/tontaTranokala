73fcd10dcee3cb7f2ad52081c4401f6b5cd5c9b6
97mams <97mams@gmail.com>
2025-08-16 12:57:20 +0300

    lou

--- server/deleteFile.ts
+++ server/deleteFile.ts
@@ -1,4 +1,3 @@
 function fafanaTy() {
     // eto no manoratra
-    return " at objet request=)creation.php artisan: name migration puble function up word.schema:create c postr;function (blueprint! and trible )  s table =)rdc ;s table=) string (tible);s table sting (slug) =)unique (); s table=) long teet(content); php artisan migrate;creat model php a makes model name model"
-}
+    return " at objet request=)creation.php artisan: name migration puble function up word.schema:create c postr;function (blueprint! and trible )  s table =)rdc ;s table=) string (tible);s table sting (slug) =)unique (); s table=) long teet(content); php artisan migrate;creat model php a makes model name model
