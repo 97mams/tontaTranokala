a48561752c885a866138d46d644bb95774a58c6a
97mams <97mams@gmail.com>
2025-08-17 14:49:54 +0300

    (t_t )

--- server/deleteFile.ts
+++ server/deleteFile.ts
@@ -1,5 +1,6 @@
 import { Content } from "@radix-ui/react-collapsible"
 import { Label } from "@radix-ui/react-label"
+import { dir } from "console"
 import { initialize } from "next/dist/server/lib/render-server.js"
 import { emoji } from "zod"
 use emoji
@@ -10,11 +11,11 @@ attr_reader:pronunceable_label
 # public: get the emoji that this reaction's content represents.
 //#endregion
 # returns an emoji.
- attr_reader :emoji_character 
+ attr_reader :emoji_character
 
  def initialize(content:, Label: nill, pronounceable-Label:nill,
         @content = Content
         @Label = label || @Content
         @pronounceable_ label || @Label
-        @emoji_ character = emoji_character || emoji. find_by- nanah @content 
+        @emoji_ character = emoji_character || emoji. find_by- 'mams_nanah' @content 
         @platform_enum = @pronounceable_label.gsub(" ", "_").upcase
