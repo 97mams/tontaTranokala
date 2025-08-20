0e00443b9de096368b05ee2af6aa2849ab330a6c
97mams <97mams@gmail.com>
2025-08-20 15:55:42 +0300

    fix: remove unused Page component for plateform

--- app/site/[plateform]/page.tsx
+++
@@ -1,6 +0,0 @@
-export default async function Page(props: {
-  params: Promise<{ plateform: string }>;
-}) {
-  const plateform = await props.params;
-  return <div>hellow{plateform.plateform}</div>;
-}
