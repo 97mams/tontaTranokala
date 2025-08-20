2446f3901fa3241fafff1f8f487824060b821bde
97mams <97mams@gmail.com>
2025-08-21 01:48:48 +0300

    fix: remove unused Page and GroupeSiteList components

--- app/site/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <div>
-      <h1>Liste site enregistrer</h1>
-      <p>Ajouter ici le nom du techno que vous voulez construire</p>
-    </div>
-  )
-}
