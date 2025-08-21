af546998e805c2da5db3182c5decf81165164070
97mams <97mams@gmail.com>
2025-08-21 20:21:16 +0300

    feat: add CardListPlateform component for displaying platform details with tooltip and actions

---
+++ src/components/cardListPlateform.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { SiteButtonActions } from "./siteButtonActions";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
+
+export async function CardListPlateform(props: {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  description: string;
+  url: string;
+  type: string;
+}) {
+  return (
+    <div className="my-8 mb-8">
+      <div className="w-full flex justify-between">
+        <Tooltip>
+          <TooltipTrigger>
+            <Link
+              href={props.url}
+              target="_blank"
+              className="text-lg font-semibold flex hover:before:content-['#'] hover:before:text-muted-foreground before:text-transparent before:content-['#'] before:mr-2"
+            >
+              {props.name}
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent>Ouvrir {props.name}</TooltipContent>
+        </Tooltip>
+        <SiteButtonActions id={props.id} data={props} />
+      </div>
+      <p className="text-muted-foreground text-sm pl-4">{props.description}</p>
+    </div>
+  );
+}
