d06bd56fc24002c9aa5b3a0fffa93b8d2c033a5f
97mams <97mams@gmail.com>
2025-08-21 20:21:08 +0300

    refactor: update CardListSite component structure and improve prop handling

--- src/components/cardListSite.tsx
+++ src/components/cardListSite.tsx
@@ -1,33 +1,32 @@
-import Link from "next/link"
-import { Tooltip, TooltipTrigger, TooltipContent } from "./ui/tooltip"
-import { SiteButtonActions } from "./siteButtonActions"
+import Link from "next/link";
+import { Tooltip, TooltipTrigger, TooltipContent } from "./ui/tooltip";
+import { SiteButtonActions } from "./siteButtonActions";
 
-export async function CardListSite(props: 
-  {
-    id: number,
-    name: string, 
-    description: string,
-    url: string
-  }
-) {
-
+export async function CardListSite(props: {
+  id: number;
+  name: string;
+  description: string;
+  url: string;
+  type: string;
+}) {
   return (
-      <div className="my-8 mb-8">
-        <div className="w-full flex justify-between">
-          <Tooltip>
-            <TooltipTrigger>
-              <Link  href={props.url} target="_blank" className="text-lg font-semibold flex hover:before:content-['#'] hover:before:text-muted-foreground before:text-transparent before:content-['#'] before:mr-2">
-                {props.name}
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent>Ouvrir {props.name}</TooltipContent>
-          </Tooltip>
-          <SiteButtonActions id={props.id} site={props}/>
-        </div>
-        <p className="text-muted-foreground text-sm pl-4">
-          {props.description}
-        </p>
+    <div className="my-8 mb-8">
+      <div className="w-full flex justify-between">
+        <Tooltip>
+          <TooltipTrigger>
+            <Link
+              href={props.url}
+              target="_blank"
+              className="text-lg font-semibold flex hover:before:content-['#'] hover:before:text-muted-foreground before:text-transparent before:content-['#'] before:mr-2"
+            >
+              {props.name}
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent>Ouvrir {props.name}</TooltipContent>
+        </Tooltip>
+        <SiteButtonActions id={props.id} data={props} />
       </div>
-  
-  )
+      <p className="text-muted-foreground text-sm pl-4">{props.description}</p>
+    </div>
+  );
 }
