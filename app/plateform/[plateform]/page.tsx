7c20be282aeaa63322f0d2ca908c167618429643
97mams <97mams@gmail.com>
2025-08-21 15:59:18 +0300

    feat: implement page component to display sites by group and update visits

--- app/plateform/[plateform]/page.tsx
+++ app/plateform/[plateform]/page.tsx
@@ -1,6 +1,46 @@
+import { CardListSite } from "@/components/cardListSite";
+import { SiteForm } from "@/components/siteForm";
+import { prisma } from "@/lib/prisma";
+import { castToString, stringToArray } from "@/lib/urlHelper";
+import { updateGroupSiteVisits } from "../../../server/group-actions";
+
 export default async function Page(props: {
   params: Promise<{ plateform: string }>;
 }) {
-  const plateform = await props.params;
-  return <div>hellow{plateform.plateform}</div>;
+  const params = await props.params;
+  const newParams = stringToArray(params.plateform);
+
+  const groupSiteId = Number(newParams[0]);
+  const sitesByGroupId = await prisma.site.findMany({
+    where: { GroupSiteId: groupSiteId },
+    select: { id: true, name: true, description: true, url: true },
+  });
+
+  await updateGroupSiteVisits(groupSiteId);
+
+  return (
+    <div className="flex flex-col gap-2">
+      <h1 className="scroll-m-20 uppercase text-4xl font-extrabold tracking-tight text-balance">
+        {castToString(newParams[1])}
+      </h1>
+      <p className="leading-7 [&:not(:first-child)]:mt-6">
+        Ici, retrouvez tous les plateforms dédiés à {castToString(newParams[1])}
+        .
+      </p>
+      <div>
+        {sitesByGroupId.map((site) => (
+          <CardListSite
+            key={site.id}
+            id={site.id}
+            name={site.name}
+            description={site.description}
+            url={site?.url ? site.url : ""}
+          />
+        ))}
+      </div>
+      <div className="flex gap-4 mt-8">
+        <SiteForm id={Number(newParams[0])} />
+      </div>
+    </div>
+  );
 }
