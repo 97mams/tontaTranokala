fcc05fe5930a9a32c54e59c4ad5aebcf2003f7a8
97mams <97mams@gmail.com>
2025-08-23 17:44:48 +0300

    feat: add update function

--- server/plateform-action.ts
+++ server/plateform-action.ts
@@ -51,3 +51,46 @@ export async function formPlateformAction(formData: FormData) {
 
   return { success: true, message: "Platform created successfully" };
 }
+
+export async function updatePlateformAction(formData: FormData) {
+  const id = Number(formData.get("id"));
+  const name = formData.get("nameplateform") as string;
+  const email = formData.get("email") as string;
+  const password = formData.get("pwd") as string;
+  const description = formData.get("description") as string;
+  const url = formData.get("urlplateform") as string;
+  const groupId = Number(formData.get("groupeplateform"));
+
+  const hashPassword = caesarCipher(password, 12);
+
+  console.log("id:", id);
+
+  const result = platefromShema.safeParse({
+    id,
+    name,
+    email,
+    passWord: hashPassword,
+    description,
+    url,
+    groupId,
+  });
+
+  if (!result.success) {
+    return { error: true, message: result.error.message };
+  }
+
+  const plateform = await prisma.plateform.update({
+    where: { id: id },
+    data: {
+      name: result.data.name,
+      email: result.data.email,
+      passWord: result.data.passWord,
+      description: result.data.description,
+      url: result.data.url,
+    },
+  });
+
+  revalidatePath("/");
+
+  return { success: true, message: "Platform updated successfully" };
+}
