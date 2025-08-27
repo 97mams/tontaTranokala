27e8fcfd69c30865730cd5bc68de32274989d041
97mams <97mams@gmail.com>
2025-08-27 18:35:48 +0300

    fix: fixed side porject and add overflow scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,40 @@
-import "./globals.css"
-import { TopProjects } from "@/components/sideListProject"
-import { AppSidebar } from "@/components/app-sidebar"
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { Toaster } from "@/components/ui/sonner"
-import { ThemeProvider } from "@/components/ui/theme-provider"
-import { ModeToggle } from "@/components/ui/theme-toggle"
+import { AppSidebar } from "@/components/app-sidebar";
+import { TopProjects } from "@/components/sideListProject";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/components/ui/theme-provider";
+import { ModeToggle } from "@/components/ui/theme-toggle";
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <ThemeProvider 
-          attribute="class" 
-          defaultTheme="treu" 
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="treu"
           enableSystem={true}
         >
           <SidebarProvider>
-            <AppSidebar/>
-                <SidebarTrigger />
-              <main className="w-full">
-               <div className="w-full flex justify-end py-2 px-2"> 
-                  <ModeToggle />
-                </div>
-                <div className="w-full flex flex-row">
-                  <div className="w-full">
-                    {children}
-                  </div>
-                  <TopProjects />
-                </div>
-              </main>
-              <Toaster />
+            <AppSidebar />
+            <SidebarTrigger />
+            <main className="w-full h-screen">
+              <div className="w-full flex justify-end py-2 px-2">
+                <ModeToggle />
+              </div>
+              <div className="w-full h-screen flex overflow-scroll">
+                {children}
+                <TopProjects />
+              </div>
+            </main>
+            <Toaster />
           </SidebarProvider>
         </ThemeProvider>
-        </body>
+      </body>
     </html>
-  )
-} 
+  );
+}
--- src/components/sideListProject.tsx
+++ src/components/sideListProject.tsx
@@ -1,25 +1,31 @@
 import { prisma } from "@/lib/prisma";
 
 export async function TopProjects() {
-
   const projects = await prisma.groupSite.findMany({
     orderBy: {
-      visits: 'desc',
+      visits: "desc",
     },
-    select: {id: true, title:true, visits: true},
-    take: 3
-  })
+    select: { id: true, title: true, visits: true },
+    take: 3,
+  });
 
-  const projectIsVisited = projects.filter(project => project.visits >= 3)
+  const projectIsVisited = projects.filter((project) => project.visits >= 3);
 
   return (
-    <div className="w-md p-4">
-      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight mb-2">Top projets</h3>
-      <p className="text-muted-foreground text-sm">Retrouvez ici vos projets les plus utilisés.</p>
+    <div className="w-md p-4 fixed z-50 right-0 flex-1">
+      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight mb-2">
+        Top projets
+      </h3>
+      <p className="text-muted-foreground text-sm">
+        Retrouvez ici vos projets les plus utilisés.
+      </p>
       <ul className="mt-4 my-6 ml-6 list-disc [&>li]:mt-2">
         {projectIsVisited.map((project) => (
           <li key={project.id}>
-            <a href={`/site/${project.id}-${project.title}`} className="hover:underline">
+            <a
+              href={`/site/${project.id}-${project.title}`}
+              className="hover:underline"
+            >
               {project.title}
             </a>
           </li>
