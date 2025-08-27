a5ff4c6163e149c1ac8febb28e4f5a4e6c7eca3f
97mams <97mams@gmail.com>
2025-08-27 16:44:13 +0300

    feat: add button show pass word

--- src/components/cardListPlateform.tsx
+++ src/components/cardListPlateform.tsx
@@ -1,8 +1,15 @@
+"use client";
+
+import { useCopyToClipboard } from "@/hooks/use-copy-to-clipboard";
+import { caesarCipher } from "@/lib/utils";
+import { Check, Copy, Eye, EyeOff } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
 import { SiteButtonActions } from "./siteButtonActions";
+import { Button } from "./ui/button";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 
-export async function CardListPlateform(props: {
+export function CardListPlateform(props: {
   id: number;
   name: string;
   email: string;
@@ -11,6 +18,16 @@ export async function CardListPlateform(props: {
   url: string;
   type: string;
 }) {
+  const [showPassword, setShowPassword] = useState(false);
+  const { isCopied: copyEmail, copyToClipboard: clicboardEmail } =
+    useCopyToClipboard();
+  const { isCopied: copyPwd, copyToClipboard: clicboardPwd } =
+    useCopyToClipboard();
+
+  const handleCopyMail = (copy: string) => clicboardEmail(copy);
+  const handleCopyPwd = (copy: string) => clicboardPwd(copy);
+  const formatPassword = caesarCipher(props.password, 12, true);
+
   return (
     <div className="my-8 mb-8">
       <div className="w-full flex justify-between">
@@ -28,7 +45,71 @@ export async function CardListPlateform(props: {
         </Tooltip>
         <SiteButtonActions id={props.id} data={props} />
       </div>
-      <p className="text-muted-foreground text-sm pl-4">{props.description}</p>
+      <div className="pl-4">
+        <p className="text-muted-foreground text-sm">{props.description}</p>
+        <h3>Adresse mail associée</h3>
+        <p className="text-muted-foreground text-sm">
+          Identifiant associé à la plateforme
+        </p>
+        <div className="relative">
+          <pre className="bg-muted p-3 rounded-md text-sm overflow-x-auto">
+            {props.email}
+          </pre>
+          <Button
+            size="sm"
+            variant="ghost"
+            onClick={() => {
+              handleCopyMail(props.email);
+            }}
+            className="absolute top-2 right-2 h-8 w-8 p-0"
+          >
+            {copyEmail ? (
+              <Check className="w-3 h-3" />
+            ) : (
+              <Copy className="w-3 h-3" />
+            )}
+          </Button>
+        </div>
+        <h3>Adresse mail associée</h3>
+        <p className="text-muted-foreground text-sm">
+          Conservez-le en sécurité, vous pouvez l’afficher ou le copier.
+        </p>
+        <div className="relative">
+          <pre className="bg-muted p-3 rounded-md text-sm overflow-x-auto">
+            <input
+              type={showPassword ? "text" : "password"}
+              defaultValue={formatPassword}
+            />
+          </pre>
+          <div className="absolute top-2 right-2 h-8 p-0 flex gap-2">
+            <Button
+              type="button"
+              variant="ghost"
+              size="sm"
+              onClick={() => setShowPassword((prev) => !prev)}
+            >
+              {showPassword ? (
+                <EyeOff className="h-4 w-4" />
+              ) : (
+                <Eye className="h-4 w-4" />
+              )}
+            </Button>
+            <Button
+              size="sm"
+              variant="ghost"
+              onClick={() => {
+                handleCopyPwd(formatPassword);
+              }}
+            >
+              {copyPwd ? (
+                <Check className="w-3 h-3" />
+              ) : (
+                <Copy className="w-3 h-3" />
+              )}
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
