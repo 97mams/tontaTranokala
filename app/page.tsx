4dacd1d15c8e25cc92c658b1f504e1e9f9e76b0d
97mams <97mams@gmail.com>
2025-09-05 16:33:15 +0300

    feat: change ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+import { GroupeSiteForm } from "@/components/groupForm";
+
 export default function Page() {
   return (
-    <div>
-      <h1>Register for link web page</h1>
-      <p>This is the main content of the page.</p>
+    <div className="flex w-full h-[calc(100vh-7rem)] border flex-col sm:flex-row">
+      <div className="container flex flex-col items-center justify-center gap-4">
+        <p className="scroll-m-20 text-2xl font-semibold tracking-tight">
+          Continue à enrichir ta collection.
+        </p>
+        <GroupeSiteForm />
+      </div>
+      <div className="w-md">{/* //void */}</div>
     </div>
   );
 }
