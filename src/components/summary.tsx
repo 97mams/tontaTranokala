0f871e3b716d6890818056edb46029b113cf509a
97mams <97mams@gmail.com>
2025-09-06 02:30:33 +0300

    feat: hadle active items

--- src/components/summary.tsx
+++ src/components/summary.tsx
@@ -1,9 +1,44 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { Card, CardContent } from "./ui/card";
 
 export function Summary(props: {
   projects: Array<{ id: number; name: string; GroupSite: { title: string } }>;
   active: number;
 }) {
+  const [activeSection, setActiveSection] = useState<String>(
+    props.projects[0].name + props.projects[0].id
+  );
+
+  console.log(activeSection === props.projects[0].name + props.projects[0].id);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      let current = props.projects[0].name + props.projects[0].id;
+      props.projects.forEach((section) => {
+        const element = document.getElementById(section.name + section.id);
+        if (element) {
+          const rect = element.getBoundingClientRect();
+          if (rect.top <= 100) current = section.name + section.id;
+        }
+      });
+      setActiveSection(current);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const scrollToSection = (sectionId: string) => {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      window.scroll({
+        top: element.offsetTop - 50,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
     <Card className="w-70 absolute right-8 top-70">
       <CardContent>
@@ -18,8 +53,21 @@ export function Summary(props: {
         </h4>
         <ul>
           {props.projects.map((project) => (
-            <li key={project.id} className="text-sm ml-3 mb-2 capitalize">
-              <a href="#">{project.name}</a>
+            <li
+              key={project.id}
+              className={`
+                text-sm ml-3 mb-2 capitalize
+              `}
+              onClick={() => scrollToSection(project.name + project.id)}
+            >
+              <a
+                href={project.name + project.id}
+                className={
+                  activeSection === project.name + project.id ? "underline" : ""
+                }
+              >
+                {project.name}
+              </a>
             </li>
           ))}
         </ul>
