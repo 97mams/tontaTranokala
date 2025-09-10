24871705a44e26067937d0fb26857a3bf25b1afc
97mams <97mams@gmail.com>
2025-09-10 15:22:36 +0300

    feat: add PlateformList component to display platform data and integrate forms

---
+++ src/components/plateformList.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { CardListPlateform } from "@/components/cardListPlateform";
+import { PlateformForm } from "@/components/plateformForm";
+import { Summary } from "@/components/summary";
+
+type plateform = {
+  id: number;
+  name: string;
+  email: string;
+  passWord: string;
+  description: string;
+  url: string | null;
+  GroupSite: {
+    id: number;
+    type: string;
+    title: string;
+  };
+};
+
+export default function PlateformList(props: {
+  data: plateform[];
+  params: { id: number; title: string };
+}) {
+  return (
+    <div className="w-full  h-screen overflow-scroll">
+      <div className="w-3xl">
+        <div className="flex flex-col gap-2">
+          <h1 className="scroll-m-20 uppercase text-4xl font-extrabold tracking-tight text-balance">
+            {props.params.title}
+          </h1>
+          {props.data.length !== 0 ? (
+            <p className="leading-7 [&:not(:first-child)]:mt-6">
+              Ici, retrouvez tous les plateforms dédiés à {props.params.title}.
+            </p>
+          ) : (
+            ""
+          )}
+          <div>
+            {props.data?.map((plateform) => (
+              <CardListPlateform
+                key={plateform.id}
+                id={plateform.id}
+                name={plateform.name}
+                email={plateform.email}
+                password={plateform.passWord}
+                description={plateform.description}
+                url={plateform?.url ? plateform.url : ""}
+                type={plateform.GroupSite.type}
+              />
+            ))}
+          </div>
+          <div className="flex gap-4 pb-20">
+            {props.data ? (
+              <PlateformForm id={Number(props.params.id)} isButton={false} />
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+        {props.data ? <Summary projects={props.data} active={1} /> : ""}
+      </div>
+    </div>
+  );
+}
