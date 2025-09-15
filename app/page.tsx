4e06d5ccc16b59bd34a2710079c5ebf10a0bc944
97mams <97mams@gmail.com>
2025-09-15 18:31:27 +0300

    fix: add space after Image component's closing tag for improved formatting

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <div className="flex w-full h-[calc(100vh-7rem)] flex-col sm:flex-row">
       <div className="container flex flex-col items-center justify-center gap-4">
-        <Image width={200} height={200} alt="log" src={"/tranokalaMd.png"}/>
+        <Image width={200} height={200} alt="log" src={"/tranokalaMd.png"} />
         <p className="scroll-m-20 text-2xl font-semibold tracking-tight">
           Continue à enrichir ta collection.
         </p>
