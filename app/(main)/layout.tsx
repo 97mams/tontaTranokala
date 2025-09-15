15fa96efc0ad67575936274a5354dd18a73fe45d
97mams <97mams@gmail.com>
2025-09-16 02:25:21 +0300

    fix:  remove tags and add header

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,10 +1,8 @@
-import { ThemeProvider } from "@/components/ui/theme-provider";
-import "./globals.css";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
-import { ModeToggle } from "@/components/ui/theme-toggle";
 import { TopProjects } from "@/components/sideListProject";
-import { Toaster } from "sonner";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { ThemeProvider } from "@/components/ui/theme-provider";
+import { ModeToggle } from "@/components/ui/theme-toggle";
 
 export default function RootLayout({
   children,
@@ -12,29 +10,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="treu"
-          enableSystem={true}
-        >
-          <SidebarProvider className="overflow-hidden">
-            <AppSidebar />
-            <main className="w-full h-screen">
-              <div className="w-full sticky z-40 flex justify-between py-2 px-2 ">
-                <SidebarTrigger />
-                <ModeToggle />
-              </div>
-              <div className="w-full max-h-screen flex">
-                {children}
-                <TopProjects />
-              </div>
-            </main>
-            <Toaster />
-          </SidebarProvider>
-        </ThemeProvider>
-      </body>
-    </html>
+    <ThemeProvider attribute="class" defaultTheme="treu" enableSystem={true}>
+      <SidebarProvider className="overflow-hidden">
+        <AppSidebar />
+        <main className="w-full h-screen">
+          <div className="w-full sticky z-40 flex justify-between py-2 px-2 ">
+            <SidebarTrigger />
+            <ModeToggle />
+          </div>
+          <div className="w-full max-h-screen flex">
+            {children}
+            <TopProjects />
+          </div>
+        </main>
+      </SidebarProvider>
+    </ThemeProvider>
   );
 }
