408c360cbd49ac221a95ce30cb391402ed25aa5e
97mams <97mams@gmail.com>
2025-09-16 18:36:42 +0300

    feat: add better auth

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from 'next'
- 
+import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
-  /* config options here */
-}
- 
-export default nextConfig
+  allowedDevOrigins: ["local-origin.dev", "*.local-oringin.dev"],
+};
+
+export default nextConfig;
---
+++ src/lib/auth.ts
@@ -0,0 +1,13 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+
+export const auth = betterAuth({
+  trustedOrigins: ["http://localhost:3000", "http://127.0.0.1:3000"],
+  database: prismaAdapter(prisma, {
+    provider: "mysql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+});
