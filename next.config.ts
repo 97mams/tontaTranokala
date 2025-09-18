ab617001aec663c65cf522129fffc64a747080a3
97mams <97mams@gmail.com>
2025-09-18 15:59:50 +0300

    feat: active expermental

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["local-origin.dev", "*.local-oringin.dev"],
+  experimental: {
+    authInterrupts: true,
+  },
 };
 
 export default nextConfig;
