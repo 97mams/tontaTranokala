c9a6b2db104e45698f3c223487e4c2a1339f0424
97mams <97mams@gmail.com>
2025-09-25 14:31:52 +0300

    fix: restore file

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,18 +1,4 @@
 import { createAuthClient } from "better-auth/react";
-import { useRouter } from "next/navigation";
 export const authClient = createAuthClient({});
 
-const router = useRouter();
-
 export const { signUp, signIn, signOut } = authClient;
-
-await authClient.signOut({
-  fetchOptions: {
-    onSuccess: () => {
-      router.push("/signin");
-    },
-    onError: (error) => {
-      console.error("Sign out error:", error);
-    },
-  },
-});
