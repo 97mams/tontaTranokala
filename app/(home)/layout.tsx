b01d4bdc6245c2d81d203652e1d7a7149014d9e3
97mams <97mams@gmail.com>
2025-09-26 18:19:27 +0300

    feat: add condition and redirect if user

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,13 +1,19 @@
 import { SignInButtonAction } from "@/components/sigInButtonAction";
 import { ModeToggle } from "@/components/ui/theme-toggle";
+import { getUser } from "@/lib/auth-server";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const user = await getUser();
+  if (user) {
+    redirect("/tranokala");
+  }
   return (
     <div className="w-full h-screen">
       <header className="border-b-2 py-2 border-accent grid grid-cols-2">
