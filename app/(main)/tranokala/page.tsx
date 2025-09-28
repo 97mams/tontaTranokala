177d186e528c28ba750e4f2441e13b14f74354a4
97mams <97mams@gmail.com>
2025-09-28 13:09:58 +0300

    feat: add paragraphe if user have a register

--- app/(main)/tranokala/page.tsx
+++ app/(main)/tranokala/page.tsx
@@ -1,5 +1,6 @@
 import { GroupeSiteForm } from "@/components/groupForm";
 import { getUser } from "@/lib/auth-server";
+import { prisma } from "@/lib/prisma";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 
@@ -8,14 +9,26 @@ export default async function Page() {
   if (!user) {
     redirect("/auth/sigin");
   }
+
+  const isGroup = await prisma.user.findUnique({
+    where: { id: user.id },
+    select: { groupeSite: { select: { id: true } } },
+  });
+
   return (
     <div className="h-[calc(100vh-7rem)] w-3xl flex flex-col items-center justify-start">
       <Image alt="logo" width={300} height={300} src={"/tranokalaMd.png"} />
       <div className="text-center">
-        <p className="leading-7 [&:not(:first-child)]:mt-6">
-          Bienvenue <span className="capitalize">{user.name}</span>. <br />{" "}
-          Ajoute ton premier enregistrement et garde tout à portée de main !
-        </p>
+        {isGroup ? (
+          <p className="leading-7 [&:not(:first-child)]:mt-6">
+            Organisez vos idées autrement : créez un nouveau groupe en un clic
+          </p>
+        ) : (
+          <p className="leading-7 [&:not(:first-child)]:mt-6">
+            Bienvenue <span className="capitalize">{user.name}</span>. <br />{" "}
+            Ajoute ton premier enregistrement et garde tout à portée de main !
+          </p>
+        )}
         <GroupeSiteForm />
       </div>
     </div>
