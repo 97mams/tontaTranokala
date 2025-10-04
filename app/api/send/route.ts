eef153af759617a464a4cabd3bfed0cb362ec727
97mams <97mams@gmail.com>
2025-10-04 09:24:16 +0300

    fix: change received mail

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,19 +1,21 @@
 import { EmailTemplate } from "@/components/email-template";
+import { getUser } from "@/lib/auth-server";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
+  const user = await getUser();
   try {
     const { data, error } = await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
-      to: ["97mams@gmail.com"],
+      from: "totanTanokala <97mams@resend.dev>",
+      to: [String(user?.email)],
       subject: "Hello world",
       react: EmailTemplate({ firstName: "John" }),
     });
 
     if (error) {
-      return Response.json({ error }, { status: 500 });
+      return Response.json({ error: error.message }, { status: 500 });
     }
 
     return Response.json(data);
