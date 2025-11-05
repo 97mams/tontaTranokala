e52d8329fae59b7d15b6c410e982a948a176bbc6
97mams <97mams@gmail.com>
2025-11-05 09:20:56 +0300

    fix: update sign-in button in dropdown menu to link for better navigation

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -43,7 +43,7 @@ export default async function RootLayout({
                 <Link href={"/docs"}>Documentation</Link>
               </DropdownMenuItem>
               <DropdownMenuItem>
-                <SignInButtonAction />
+                <Link href={"/auth/signin"}>Se connecter</Link>
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
