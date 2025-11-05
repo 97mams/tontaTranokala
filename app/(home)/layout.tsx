8ccc4e7e3e2f9e8d020ac8227c96295c038fb306
97mams <97mams@gmail.com>
2025-11-05 08:25:20 +0300

    fix: refactor header layout for improved responsiveness and structure

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,8 +1,15 @@
 import { Footer } from "@/components/footer";
 import { SignInButtonAction } from "@/components/sigInButtonAction";
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { ModeToggle } from "@/components/ui/theme-toggle";
 import { getUser } from "@/lib/auth-server";
+import { Menu } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
@@ -18,13 +25,31 @@ export default async function RootLayout({
   }
   return (
     <div className="w-full h-screen">
-      <header className="border-b-2 py-2 border-accent grid grid-cols-2">
-        <div className="flex items-center pl-60 ">
+      <header className="border-b-2 py-2 border-accent flex justify-between p-4  md:grid md:grid-cols-2">
+        <div className="flex items-center pl-2 md:pl-60 ">
           <Link href={"/"}>
             <Image width={30} height={30} alt="logo" src={"/tranokala.png"} />
           </Link>
         </div>
-        <div className="flex justify-end gap-1  pr-60">
+        <div className="md:hidden flex items-center gap-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button>
+                <Menu />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>
+                <Link href={"/docs"}>Documentation</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <SignInButtonAction />
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <ModeToggle />
+        </div>
+        <div className="hidden md:flex justify-end gap-1 md:pr-60">
           <Link href={"/docs"}>
             <Button variant={"outline"}>Documentation</Button>
           </Link>
