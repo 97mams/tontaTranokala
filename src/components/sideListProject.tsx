9bfbc0028d4ddf0bd1e71901ff91f63ab79f0892
97mams <97mams@gmail.com>
2025-11-07 20:20:30 +0300

    fix: remove hidden class from TopProjects component for improved visibility

--- src/components/sideListProject.tsx
+++ src/components/sideListProject.tsx
@@ -20,7 +20,7 @@ export function TopProjects(props: { userId: string }) {
   }
 
   return (
-    <div className="hidden md:block md:w-[20rem] p-4 fixed right-0 flex-1">
+    <div className="md:w-[20rem] p-4 fixed right-0 flex-1">
       <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight mb-2">
         Top projets
       </h3>
