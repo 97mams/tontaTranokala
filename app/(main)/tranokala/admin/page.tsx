3d013032b2ff5e2e3cc1b37497bea6a0fa0d64e0
97mams <97mams@gmail.com>
2025-11-15 19:31:54 +0300

    feat: restructure Page component layout for improved readability

--- app/(main)/tranokala/admin/page.tsx
+++ app/(main)/tranokala/admin/page.tsx
@@ -1,3 +1,10 @@
 export default function Page() {
-  return <div>Tranokala Admin Page</div>;
+  return (
+    <div>
+      <div className="w-dm border-r-accent border-r-2">
+        Tranokala Admin Page
+      </div>
+      <div className="w-full bg-red-700"></div>
+    </div>
+  );
 }
