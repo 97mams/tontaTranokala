bf8d5f7e942c81960472aee3c4819d533c3f45fd
97mams <97mams@gmail.com>
2025-11-19 19:35:02 +0300

    fix: add additional trusted origin and session configuration in auth setup

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,10 +3,14 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
 
 export const auth = betterAuth({
-  trustedOrigins: ["http://localhost:3000"],
+  trustedOrigins: ["http://localhost:3000", "http://localhost:3001"],
   database: prismaAdapter(prisma, {
     provider: "mysql",
   }),
+  session: {
+    expiresIn: 60 * 60 * 24 * 1, //1 day
+    updateAge: 60 * 60,
+  },
   emailAndPassword: {
     enabled: true,
   },
