25007f54efd73081d5b54e3ccd42c1b21a99df33
97mams <97mams@gmail.com>
2025-11-25 18:54:20 +0300

    feat: implements components admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,4 @@
+import { Chart } from "./_components/chart";
 import { Sidebar } from "./_components/sidebar";
 import { Users } from "./_components/users";
 
@@ -5,8 +6,9 @@ export default function Page() {
   return (
     <div className="flex w-full h-screen">
       <Sidebar />
-      <div className="w-full pt-8 px-20">
+      <div className="w-full pt-8 px-20 flex flex-col gap-4">
         <Users />
+        <Chart />
       </div>
     </div>
   );
