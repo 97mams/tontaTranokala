a777b124616155d64bd20acd6cfb960b115127e2
97mams <97mams@gmail.com>
2025-11-28 19:50:36 +0300

    fix: add max heith in parent and overflow scroll

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -5,9 +5,9 @@ import { Users } from "./_components/users";
 
 export default function Page() {
   return (
-    <div className="flex w-full h-screen">
+    <div className="flex w-full max-h-screen">
       <Sidebar />
-      <div className="w-full pt-8 px-20 flex flex-col gap-4">
+      <div className="w-full overflow-scroll pt-8 px-20 flex flex-col gap-4">
         <Users />
         <Chart />
         <ListUsers />{" "}
