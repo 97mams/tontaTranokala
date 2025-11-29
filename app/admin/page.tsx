95affdd6e6a18d80d10c7612ae92187737973c89
97mams <97mams@gmail.com>
2025-11-29 19:56:16 +0300

    feat: get users

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,16 +1,23 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
 import { Chart } from "./_components/chart";
 import { ListUsers } from "./_components/listUsers";
 import { Sidebar } from "./_components/sidebar";
 import { Users } from "./_components/users";
 
-export default function Page() {
+export default async function Page() {
+  const users = await prisma.user.findMany({
+    select: { id: true, name: true, email: true },
+  });
+
   return (
     <div className="flex w-full max-h-screen">
       <Sidebar />
       <div className="w-full overflow-scroll pt-8 px-20 flex flex-col gap-4">
         <Users />
         <Chart />
-        <ListUsers />{" "}
+        <ListUsers data={users} />{" "}
       </div>
     </div>
   );
