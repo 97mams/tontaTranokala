2b4a99c4f2a59edc4d8f4dc81a53c9eb08adedf2
97mams <97mams@gmail.com>
2025-12-05 13:28:07 +0300

    refactor: streamline data retrieval and formatting functions

--- server/admin.ts
+++ server/admin.ts
@@ -1,65 +1,45 @@
-"use server";
-
 import { prisma } from "@/lib/prisma";
 
-type chartData = {
-  day: string;
-  plateform: string;
-  site: string;
+type data = {
+  id: number;
+  createdAt: Date;
+}[];
+
+async function getPlateform() {
+  const plateform = await prisma.plateform.findMany({
+    select: { id: true, createdAt: true },
+  });
+  return plateform;
+}
+
+async function getSite() {
+  const site = await prisma.site.findMany({
+    select: { id: true, createdAt: true },
+  });
+  return site;
+}
+
+export const countIdbyCreateAt = async () => {
+  const plateformData = await getPlateform();
+  const siteData = await getSite();
+
+  const formattedPlateformData = setData(plateformData);
+  const formattedSiteData = setData(siteData);
+  return { plateform: formattedPlateformData, site: formattedSiteData };
 };
 
-// export async function chartData(): chartData[] {}
-
-export async function dataChart() {
-  const site: chartData[] = await prisma.$queryRaw`
-     SELECT
-    DATE(createdAt) AS day,
-    COUNT(*) AS site
-  FROM site
-  GROUP BY DATE(createdAt)
-  ORDER BY day;
-`;
-  console.log(site);
-
-  const plateform: chartData[] = await prisma.$queryRaw`
-     SELECT
-    DATE(createdAt) AS day,
-    COUNT(*) AS plateform
-  FROM plateform
-  GROUP BY DATE(createdAt)
-  ORDER BY day;
-`;
-
-  return chartDataChema(plateform, site);
-}
-
-/**
- *  format for data chart
- * @param plateform:String
- * @param site:Number
- * @returns
- */
-function chartDataChema(
-  plateform: Array<{ day: string; plateform: string }>,
-  site: Array<{ day: string; site: string }>
-) {
-  const castPlateform = plateform.map(Object.values);
-  const castSite = site.map(Object.values);
-
-  console.log(castPlateform);
-  const result: any[] = [];
-  for (let i = 0; i < castPlateform.length; i++) {
-    const p = new Set(castPlateform[i]);
-    const s = new Set(castSite[i]);
-    s.union(p);
+function setData(data: data) {
+  const array: any[] = [];
+  for (let i = 0; i < data.length; i++) {
+    const formattedDate = DayMonthYear(data[i].createdAt);
+    array.push({ id: data[i].id, createdAt: formattedDate });
   }
-
-  console.log("result", result);
-  return [];
+  return array;
 }
 
-function getDATE(date: string) {
-  const d = new Date(date);
-  const newD = d.getDay() + "-" + d.getMonth() + "-" + d.getFullYear();
-  return newD;
+function DayMonthYear(date: Date) {
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const year = date.getFullYear();
+  return `${day}-${month}-${year}`;
 }
