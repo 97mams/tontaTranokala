f42e87f6e6f3593c3f963af42bca5f1ddb6d4110
97mams <97mams@gmail.com>
2025-12-05 17:17:13 +0300

    refactor: enhance data formatting and structure for plateform and site

--- server/admin.ts
+++ server/admin.ts
@@ -5,6 +5,13 @@ type data = {
   createdAt: Date;
 }[];
 
+type dataChart =
+  | {
+      plateform: number;
+      createdAt: Date;
+    }
+  | { site: number; createdAt: Date }[];
+
 async function getPlateform() {
   const plateform = await prisma.plateform.findMany({
     select: { id: true, createdAt: true },
@@ -23,10 +30,34 @@ export const countIdbyCreateAt = async () => {
   const plateformData = await getPlateform();
   const siteData = await getSite();
 
-  const formattedPlateformData = setData(plateformData);
-  const formattedSiteData = setData(siteData);
-  return { plateform: formattedPlateformData, site: formattedSiteData };
+  const formattedPlateformData = setData(plateformData).map((item) => ({
+    plateform: item.id,
+    date: item.createdAt,
+  }));
+  const formattedSiteData = setData(siteData).map((item) => ({
+    site: item.id,
+    date: item.createdAt,
+  }));
+  return flatData([...formattedPlateformData, ...formattedSiteData]);
 };
+
+function flatData(data: dataChart) {
+  const result: any[] = [];
+  console.log("data", data);
+  for (let index = 0; index < data.length - 1; index++) {
+    let id = 1;
+    if (data[index].createdAt === data[index + 1]?.createdAt) {
+      console.log("true");
+      id += 1;
+    } else if (data[index].createdAt === data[index - 1]?.createdAt) {
+      continue;
+    } else {
+      id = 1;
+    }
+    result.push({ id: id, createdAt: data[index].createdAt });
+  }
+  return result;
+}
 
 function setData(data: data) {
   const array: any[] = [];
