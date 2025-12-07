96003b918b1cd3a816ef267fff495f8f2fcc598f
97mams <97mams@gmail.com>
2025-12-07 17:16:41 +0300

    refactor: clean up ChartRadialLabel component by removing unused imports and comments

--- app/admin/_components/user/Radical.Chart.tsx
+++ app/admin/_components/user/Radical.Chart.tsx
@@ -2,30 +2,13 @@
 
 import { LabelList, RadialBar, RadialBarChart } from "recharts";
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import {
   ChartConfig,
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
-
-export const description = "A radial chart with a label";
-
-const chartData = [
-  { type: "chrome", registers: 275, fill: "var(--color-chrome)" },
-  { type: "safari", registers: 200, fill: "var(--color-safari)" },
-  { type: "firefox", registers: 187, fill: "var(--color-firefox)" },
-  { type: "edge", registers: 173, fill: "var(--color-edge)" },
-  { type: "other", registers: 90, fill: "var(--color-other)" },
-];
 
 const chartConfig = {
   registers: {
@@ -45,10 +28,6 @@ export function ChartRadialLabel(props: { user: any[] }) {
   const chartData = props.user;
   return (
     <Card className="flex flex-col">
-      <CardHeader className="items-center pb-0">
-        <CardTitle>Radial Chart - Label</CardTitle>
-        <CardDescription>January - June 2024</CardDescription>
-      </CardHeader>
       <CardContent className="flex-1 pb-0">
         <ChartContainer
           config={chartConfig}
