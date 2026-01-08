9bf79555502bbca7c9ab9e1584a2d9f172f39e80
97mams <97mams@gmail.com>
2026-01-08 08:58:48 +0300

    refactor: add card shadcnui

--- src/components/cardListSite.tsx
+++ src/components/cardListSite.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { SiteButtonActions } from "./siteButtonActions";
+import { Card, CardContent } from "./ui/card";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 
 export async function CardListSite(props: {
@@ -11,22 +12,28 @@ export async function CardListSite(props: {
 }) {
   return (
     <div className="my-8 mb-8">
-      <div className="w-full flex justify-between">
-        <Tooltip>
-          <TooltipTrigger>
-            <Link
-              href={props.url}
-              target="_blank"
-              className="text-lg font-semibold flex hover:before:content-['#'] hover:before:text-muted-foreground before:text-transparent before:content-['#'] before:mr-2"
-            >
-              {props.name}
-            </Link>
-          </TooltipTrigger>
-          <TooltipContent>Ouvrir {props.name}</TooltipContent>
-        </Tooltip>
-        <SiteButtonActions id={props.id} data={props} />
-      </div>
-      <p className="text-muted-foreground text-sm pl-4">{props.description}</p>
+      <Card>
+        <CardContent>
+          <div className="w-full flex justify-between">
+            <Tooltip>
+              <TooltipTrigger>
+                <Link
+                  href={props.url}
+                  target="_blank"
+                  className="text-lg font-semibold flex hover:before:content-['#'] hover:before:text-muted-foreground before:text-transparent before:content-['#'] before:mr-2"
+                >
+                  {props.name}
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>Ouvrir {props.name}</TooltipContent>
+            </Tooltip>
+            <SiteButtonActions id={props.id} data={props} />
+          </div>
+          <p className="text-muted-foreground text-sm pl-4">
+            {props.description}
+          </p>
+        </CardContent>
+      </Card>
     </div>
   );
 }
