faebbed1e146c6513d76b28243784a1ebe039ec0
97mams <97mams@gmail.com>
2026-07-31 20:29:34 +0300

    refactor: update package dependencies, enhance router setup with Convex integration, and implement user password update mutation

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,46 @@
-import { createRouter as createTanStackRouter } from '@tanstack/react-router'
+import { createRouter, createRouter as createTanStackRouter } from '@tanstack/react-router'
+import { notifyManager, QueryClient } from '@tanstack/react-query'
+import { setupRouterSsrQueryIntegration } from '@tanstack/react-router-ssr-query'
+import { ConvexQueryClient } from '@convex-dev/react-query'
 import { routeTree } from './routeTree.gen'
 
 export function getRouter() {
-  const router = createTanStackRouter({
+  if(typeof document !== 'undefined') {
+    notifyManager.setScheduler(window.requestAnimationFrame)
+  }
+
+  const convexUrl = (import.meta as any).env.VITE_CONVEX_URL!
+
+  if(convexUrl) {
+    throw new Error('VITE_CONVEX_UTL is not set')
+  }
+  const convexQueryClient = new ConvexQueryClient(convexUrl, {
+    expectAuth: true,
+  })
+
+  const queryClient: QueryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        queryKeyHashFn: convexQueryClient.hashFn(),
+        queryFn: convexQueryClient.queryFn(),
+      }
+    }
+  })
+
+  convexQueryClient.connect(queryClient)
+
+  const router = createRouter({
     routeTree,
-    scrollRestoration: true,
     defaultPreload: 'intent',
-    defaultPreloadStaleTime: 0,
+    context: { queryClient, convexQueryClient },
+    scrollRestoration: true,
+    defaultErrorComponent: (err) => <p>{err.error.stack}</p>,
+    defaultNotFoundComponent: () => <p>not found</p>,
+  })
+
+  setupRouterSsrQueryIntegration({
+    router,
+    queryClient,
   })
 
   return router
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,17 +1,40 @@
-import { HeadContent, Scripts, createRootRoute, redirect } from '@tanstack/react-router'
+import { HeadContent, Outlet, Scripts, createRootRouteWithContext, useRouteContext } from '@tanstack/react-router'
+import { createServerFn } from '@tanstack/react-start'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 
 import ConvexProvider from '../integrations/convex/provider'
+import { ConvexBetterAuthProvider } from '@convex-dev/better-auth/react'
+import type { ConvexQueryClient } from '@convex-dev/react-query'
+import type { QueryClient } from '@tanstack/react-query'
+import { authClient } from '@/lib/auth-client'
+import { getToken } from '@/lib/auth-server'
 
 import appCss from '../styles.css?url'
 
 import { ThemeProvider } from "@/components/theme-provider"
 import { Toaster } from '#/components/ui/sonner'
 
-export const Route = createRootRoute({
+const getAuth = createServerFn({ method: 'GET' }).handler(async () => {
+  return await getToken()
+})
+
+export const Route = createRootRouteWithContext<{queryClient: QueryClient, convexQueryClient: ConvexQueryClient}>()({
+  beforeLoad: async (ctx) => {
+    const token = await getAuth()
+
+    if(token) {
+      ctx.context.convexQueryClient.serverHttpClient?.setAuth(token)
+    }
+
+    return {
+      isAuthenticated : !!token,
+      token,
+    }
+
+  },
   head: () => ({
     meta: [
       {
@@ -32,8 +55,23 @@ export const Route = createRootRoute({
       },
     ],
   }),
-  shellComponent: RootDocument,
+  shellComponent: RootComponent,
 })
+
+function RootComponent () {
+  const context = useRouteContext({from: Route.id})
+  return (
+    <ConvexBetterAuthProvider
+      client={context.convexQueryClient.convexClient}
+      authClient={authClient}
+      initialToken={context.token}
+    >
+      <RootDocument>
+        <Outlet />
+      </RootDocument>
+    </ConvexBetterAuthProvider>
+  )
+}
 
 function RootDocument({ children }: { children: React.ReactNode }) {
 
