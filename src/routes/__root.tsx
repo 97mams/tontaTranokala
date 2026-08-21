c5ced6d92ecbd79e9a4f00d0e226000beaff867b
97mams <97mams@gmail.com>
2026-08-21 20:30:57 +0300

    refactor: remove console log from RootDocument component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -69,7 +69,7 @@ function RootComponent() {
 function RootDocument({ children }: { children: React.ReactNode }) {
 
   const context = useRouteContext({ from: Route.id })
-  console.log(context)
+  
   const renderDefault = () => {
     return (
       <>
