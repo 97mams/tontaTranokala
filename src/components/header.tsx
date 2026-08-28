61ed8928c86610f4afd1423dbbab566153d0d14b
97mams <97mams@gmail.com>
2026-08-28 20:55:39 +0300

    feat: add div :)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,9 @@
 export function Header () {
     return(
-        <div className="h-25 w-12 flex justify-center items-center">
-            
+        <div className="h-25 w-12 flex justify-end items-center">
+			<div className="flex gap-2">
+				
+			</div>            
         </div>
     )
 }
